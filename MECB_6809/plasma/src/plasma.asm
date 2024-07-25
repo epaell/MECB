96c298f80e5f6a67a9fcadf512816fb72b03a1b0
epaell <elenc@me.com>
2024-07-26 07:46:04 +1000

    Updated for MECB

--- MECB_6809/plasma/src/plasma.asm
+++ MECB_6809/plasma/src/plasma.asm
@@ -1,186 +1,214 @@
-; vim:noet:sw=8:ts=8:ai:syn=as6809
-; Matt Sarnoff (msarnoff.org/6809) - May 13, 2010
+; Based on code from Matt Sarnoff (msarnoff.org/6809)
 ;
 ; Plasma effect.
 
-	.nlist
-	.include "../../rom/6809.inc"
-	.include "../../rom/rom06.inc"
-	.list
+;
+                INCLUDE "src/mecb.inc"
+                INCLUDE "src/ASSISTMacros.inc"
 
-	;.define SOUND
+SOUND           EQU     0       ; Don't include sound handling yet
 
 ; VRAM addresses (for Graphics I)
-SPRPATTABLE	.equ	0x0000
-PATTABLE	.equ	0x0800
-SPRATTABLE	.equ	0x1000
-NAMETABLE	.equ	0x1400
-COLORTABLE	.equ	0x2000
+SPRPATTABLE     EQU     0x0000
+PATTABLE        EQU     0x0800
+SPRATTABLE      EQU     0x1000
+NAMETABLE       EQU     0x1400
+COLORTABLE      EQU     0x2000
 
 ; Parameters
-GRID_WIDTH	.equ	32
-GRID_HEIGHT	.equ	34
-GRID_SIZE	.equ	GRID_WIDTH*GRID_HEIGHT
-NUM_COLORS	.equ	8
+GRID_WIDTH      EQU     32
+GRID_HEIGHT     EQU     24          ; ELENC was 34
+GRID_SIZE       EQU     GRID_WIDTH*GRID_HEIGHT
+NUM_COLORS      EQU     8
+ACIA            EQU     $C008       ; MC6851 ACIA base address
 
 ;------------------------------------------------------------------------------
 ; variables
 ;------------------------------------------------------------------------------
 
 ; variables in direct page
-VARSTART	.equ	0x80
+                SETDP	#$00
+
+VARSTART        ORG     $0080
 
 ; grid pointers
-CURRENTGRID	.equ	VARSTART	;pointer to GRID1 or GRID2
-NEXTGRID	.equ	VARSTART+2	;pointer to GRID1 or GRID2
+CURRENTGRID     RMB     2
+NEXTGRID        RMB     3       ; Pointer to GRID1/GRID2
 
-T		.equ	VARSTART+5
-T_3		.equ	VARSTART+6
-DIV3_COUNT	.equ	VARSTART+7
-SIN_T		.equ	VARSTART+8
-SIN_T_3		.equ	VARSTART+9
+T               RMB     1
+T_3             RMB     1
+DIV3_COUNT      RMB     1
+SIN_T           RMB     1
+SIN_T_3         RMB     1
 
 ; evaluation function pointer
-PLASMA_FN	.equ	VARSTART+10
+PLASMA_FN       RMB     2
 
 ; temporary values usable by computation functions
-TEMP1		.equ	VARSTART+12
-TEMP2		.equ	VARSTART+13
+TEMP1           RMB     1
+TEMP2           RMB     1
+HEX16           RMB     2
 
 ;------------------------------------------------------------------------------
 ; setup
 ;------------------------------------------------------------------------------
-	.area	_CODE(ABS)
-	.org	USERPROG_ORG
-		lds	#RAMEND+1	;set up stack pointer
-		ldd	#VBLANK		;set up interrupt vector
-		std	IRQVEC
-		jsr	VDP_CLEAR	;clear VRAM
-		ldx	#vdp_regs	;initialize VDP registers
-		jsr	VDP_SET_REGS
+                org     USERPROG_ORG
+;
+                CLRA                    ; Initialise Direct Page Register for Zero page
+                TFR     A,DP
+
+                LEAX    VBLANK,PCR
+                vctrsw  _IRQ
+
+                JSR     VDP_CLEAR       ; clear VRAM
+                LDX     #vdp_regs       ; initialize VDP registers
+                JSR     VDP_SET_REGS
 
 ; set up the pattern table
-		ldd	#(VRAM|PATTABLE)
-		stb	VDP_REG
-		sta	VDP_REG
-		lda	#NUM_COLORS	;write 8 copies of the cell patterns
-		pshs	a
-loadcellpats:	ldx	#CELLPATS
-		ldb	#16
-		jsr	VDP_LOADPATS
-		dec	,s
-		bne	loadcellpats
-		puls	a
+                LDD     #(VRAM|PATTABLE)
+                STB     VDP_REG
+                STA     VDP_REG
+                LDA     #NUM_COLORS     ; write 8 copies of the cell patterns
+                PSHS    A
+loadcellpats    LDX     #CELLPATS
+                LDB     #16
+                JSR     VDP_LOADPATS
+                DEC     ,S
+                BNE     loadcellpats
+                PULS    A
 
 ; set up the color table
-		ldd	#(VRAM|COLORTABLE)
-		stb	VDP_REG
-		sta	VDP_REG
-		ldx	#COLORS
-		ldb	#2		;2*8 = 16 bytes
-		jsr	VDP_LOADPATS
+                LDD     #(VRAM|COLORTABLE)
+                STB     VDP_REG
+                STA     VDP_REG
+                LDX     #COLORS
+                LDB     #2              ; 2*8 = 16 bytes
+                JSR     VDP_LOADPATS
+
+; Clear grids
+                LDX     #GRID1
+                LDY     #GRID_SIZE
+clear_grid1     CLR     ,X+
+                LEAY    -1,Y
+                BNE     clear_grid1
+                LDX     #GRID2
+                LDY     #GRID_SIZE
+clear_grid2     CLR     ,X+
+                LEAY    -1,Y
+                BNE     clear_grid2
 
 ; set up variables
-		ldd	#GRID1
-		std	*CURRENTGRID
-		ldd	#GRID2
-		std	*NEXTGRID
-		clr	*T
-		clr	*T_3
-		lda	#3
-		sta	*DIV3_COUNT
-		ldd	#WAVE2
-		std	*PLASMA_FN
+                LDD     #GRID1
+                STD     CURRENTGRID
+                LDD     #GRID2
+                STD     NEXTGRID
+                CLR     T
+                CLR     T_3
+                LDA     #3
+                STA     DIV3_COUNT
+                LDD     #WAVE2
+                STD     PLASMA_FN
 
-	.ifdef	SOUND
 ; set up sound
-		ldd	#(PSG_CTRL<<8)|TONE_AB|NOISE_NONE
-		std	PSG
-		ldd	#(PSG_A_AMPL<<8)|15;ENV_ENABLE
-		std	PSG
-		ldd	#(PSG_B_AMPL<<8)|15;ENV_ENABLE
-		std	PSG
-
-		ldd	#(PSG_ENV_SHAPE<<9)|8
-		std	PSG
-	.endif
+                IF SOUND
+                JSR     stg_init
+                LDA     #$01              ; Turn on channel 0
+                JSR     stg_atten0
+                LDA     #$01              ; Turn on channel 1
+                JSR     stg_atten1
+                ENDIF
 
 ; enable interrupts
-		andcc	#0b11101111
+                ANDCC   #0b11101111         ; Enable IRQ Interrupts
 
 ; turn on the display, enable vertical blanking interrupt
-		ldd	#0xE081		;set bits 6 and 5 of register 1
-		sta	VDP_REG
-		stb	VDP_REG
-		jmp	loop
+                LDD     #0xE081             ; set bits 6 and 5 of register 1
+                STA     VDP_REG
+                STB     VDP_REG
+                JMP     loop
 
 
 ;------------------------------------------------------------------------------
 ; logic update routine
 ;------------------------------------------------------------------------------
-loop:	
+loop	
 ; iterate and generate NEXTGRID
-		ldy	*NEXTGRID
-		ldd	#(32<<8)|24	;initialize x and y counters
-		pshs	d
+                LDY     NEXTGRID
+                LDD     #(GRID_WIDTH<<8)|GRID_HEIGHT ; initialize x and y counters
+                PSHS    D
 
-		ldu	#SIN8
-		ldd	*T		;take sin of T and T/3
-		lda	a,u
-		ldb	b,u
-		std	*SIN_T
+                LDU     #SIN8
+                LDD     T           ; take sin of T and T/3
+                LDA     A,U
+                LDB     B,U
+                STD     SIN_T
 
 ; x is in ,s and y is in 1,s
-yloop:
+yloop
 ;---- inner loop
-		lda	#32
-		sta	,s
+                LDA     #GRID_WIDTH
+                STA     ,S
 ;-------- calculation
-xloop:		jmp	[PLASMA_FN]
-setcell:	anda	#0b01111111
-		sta	,y+
+xloop           JMP     [PLASMA_FN]
+setcell         ANDA    #0b01111111
+                STA     ,Y+
 ;-------- end calculation
-		dec	,s
-		bne	xloop
+                DEC     ,S
+                BNE     xloop
 ;---- end inner loop
-		dec	1,s
-		bne	yloop
+                DEC     1,S
+                BNE     yloop
 
 ; advance timers
-		inc	*T
-		dec	*DIV3_COUNT
-		bne	flipbuffers
-		inc	*T_3
+                INC     T
+                DEC     DIV3_COUNT
+                BNE     flipbuffers
+                INC     T_3
 
 ; flip buffers
-flipbuffers:	ldx	*CURRENTGRID
-		ldu	*NEXTGRID
-		stu	*CURRENTGRID
-		stx	*NEXTGRID
+flipbuffers     LDX     CURRENTGRID
+                LDU     NEXTGRID
+                STU     CURRENTGRID
+                STX     NEXTGRID
 
-	.ifdef	SOUND
 ; update sound
-		;lda	#0x03
-		clrb
-		ldx	#GRID1
-		;ldb	*SIN_T_3
-		lda	,x
-		jsr	PSG_SET_AFREQ
+                IF      SOUND
+                PSHS    A,B,X
+                CLRA
+                LDB     SIN_T
+                ADDD    #$180
+                LSLB
+                ROLA
+                LSLB
+                ROLA
+                TFR     D,X
+                JSR     stg_freq0
 
-		lda	#0x00
-		ldx	#GRID1+400
-		;ldb	*SIN_T
-		ldb	,x
-		negb
-		jsr	PSG_SET_BFREQ
-
-		;ldb	*SIN_T_3
-		;ldb	GRID1+234
-		;lda	#1
-		;jsr	PSG_SET_EFREQ
-	.endif
-		sync
-		jmp	loop
+                CLRA
+                LDB	    GRID1
+                LSLB
+                ROLA
+                LSLB
+                ROLA
+                LSLB
+                ROLA
+                TFR     D,X
+                JSR     stg_freq1
+                PULS    X,B,A
+                ENDIF
+                
+                LBSR    CIDTA
+                BCC     continue
+                CMPA    #'q'
+                BEQ     exit
+continue        SYNC
+                JMP     loop
+;
+exit            
+                IF      SOUND
+                LBSR    stg_stop
+                ENDIF
+                monitr  #$01            ; Return to ASSIST09
 
 
 ;------------------------------------------------------------------------------
@@ -192,124 +220,133 @@ flipbuffers:	ldx	*CURRENTGRID
 ; return value in a
 ; do not rts, branch to setcell
 
-GRADIENT:	lda	,s
-		adda	1,s
-		adda	*T
-		bra	setcell
+GRADIENT        LDA     ,S
+                ADDA    1,S
+                ADDA    T
+                BRA     setcell
 
-MUNCHING:	lda	,s
-		deca
-		eora	1,s
-		adda	*T
-		bra	setcell
+MUNCHING        LDA     ,S
+                DECA
+                EORA    1,S
+                ADDA    T
+                BRA     setcell
 
-WAVE:		lda	,s
-		adda	*T
-		ldu	#SIN8
-		lda	a,u
-		adda	1,s
-		suba	*T
-		lda	a,u
-		bra	setcell
+WAVE            LDA     ,S
+                ADDA    T
+                LDU     #SIN8
+                LDA     A,U
+                ADDA    1,S
+                SUBA    T
+                LDA     A,U
+                LBRA    setcell
 
-WAVE2:		lda	,s		;first component, sin(y)
-		adda	*T_3
-		lda	a,u
-		adda	*T
-		;lsla
-		lda	a,u
-		sta	*TEMP1
+WAVE2           LDA     ,S          ; first component, sin(y)
+                ADDA    T_3
+                LDA     A,U
+                ADDA    T
+                LDA     A,U
+                STA     TEMP1
 
-		lda	1,s
-		adda	*T
-		lda	a,u
-		adda	*T_3
-		;lsla
-		lda	a,u
+                LDA     1,S
+                ADDA    T
+                LDA     A,U
+                ADDA    T_3
+                LDA     A,U
 
-		adda	*TEMP1
-		bra	setcell
-
-		
-
+                ADDA    TEMP1
+                LBRA    setcell
 
 ;------------------------------------------------------------------------------
 ; vertical blanking interrupt handler
 ;------------------------------------------------------------------------------
-VBLANK:		lda	VDP_REG		;read status, clear interrupt flag
+VBLANK          LDA     VDP_REG     ; read status, clear interrupt flag
 ; copy the grid into the name table
-		ldd	#(VRAM|NAMETABLE)
-		stb	VDP_REG
-		sta	VDP_REG
-		ldu	*CURRENTGRID
-		ldx	#VDP_VRAM
+                LDD     #(VRAM|NAMETABLE)
+                STB     VDP_REG
+                STA     VDP_REG
+                LDU     CURRENTGRID
+                LDX     #VDP_VRAM
+                LDY     #GRID_WIDTH*GRID_HEIGHT/2
 ; stack-blast the grid into VRAM (pulu d is faster than ldd ,u++)
-	.rept	16*24		;unroll that shit
-		pulu	d
-		sta	,x
-		stb	0,x		;extra cycle added so VDP doesn't miss
-	.endm
-		rti		
-	
+vbloop          PULU    D
+                STA     ,X
+                vdp_wait
+                STB     ,X
+                LEAY   -1,Y
+                BNE    vbloop
+                RTI
 
 ;------------------------------------------------------------------------------
 ; subroutines
 ;------------------------------------------------------------------------------
 
+;
+delay1MS
+; Function:	Delay 1ms (Approximately. Actually 1.004ms at 1Mhz clock)
+; Parameters:	-
+; Returns:	-
+; Destroys:	X, Y
+                LDX     #1              ; 3 Cycles
+DelayMSLoop	    LDY     #123            ; 4 Cycles - Assumes 1Mhz Clock
+Delay1MSLoop	LEAY    -1,Y            ; 5 cycles
+                BNE     Delay1MSLoop    ; 3 cycles
+                LEAX    -1,X            ; 5 cycles
+                BNE     DelayMSLoop     ; 3 cycles
+                RTS                     ; 5 cycles
+
+delayMS
+; Function:	Delay X ms (Actually X * 1.004ms + 0.003ms at 1Mhz clock)
+; Parameters:	X - Specifies desired delay in millseconds (note above)
+; Returns:	-
+; Destroys:	X, Y
+                BRA	    DelayMSLoop     ; 3 cycles
+
+; Check for character in receive buffer
+CIDTA           LDA     ACIA            ; LOAD STATUS REGISTER
+                LSRA                    ; TEST RECEIVER REGISTER FLAG
+                BCC     CIRTN           ; RETURN IF NOTHING
+                LDA     ACIA+1          ; LOAD DATA BYTE
+CIRTN           RTS                     ; RETURN TO CALLER
 
 ;------------------------------------------------------------------------------
 ; includes
 ;------------------------------------------------------------------------------
 
-	.include "../include/random.asm"
-	.include "../include/ym2149.asm"
+                INCLUDE "src/vdp.asm"
+                IF SOUND
+                INCLUDE "src/stg.asm"
+                ENDIF
 
-
-;------------------------------------------------------------------------------
-; static data
-;------------------------------------------------------------------------------
-
-; VDP register values
-vdp_regs:	.fcb	0x00	;Graphics I
-		.fcb	0x80	;Graphics I, 16K, display off, no sprites
-		.fcb	NAMETABLE/0x0400
-		.fcb	COLORTABLE/0x0040
-		.fcb	PATTABLE/0x0800
-		.fcb	SPRATTABLE/0x0080
-		.fcb	SPRPATTABLE/0x0800
-		.fcb	0x01	;black background
-
-; trig tables
-		.include "sin8.inc"
-;COS8:		.include "cos8.inc"
+; trig table
+                INCLUDE "src/sin8.inc"
 
 ; cell graphics
-CELLPATS:	.include "cells16.inc"
-CELLPATS_END	.equ	.
+CELLPATS        INCLUDE "src/cells16.inc"
+CELLPATS_END    EQU     *
 
 ; color order:
-; medium red (0x8)
-; light red (0x9)
+; medium red   (0x8)
+; light red    (0x9)
 ; light yellow (0xB)
-; light green (0x3)
-; cyan (0x7)
-; light blue (0x5)
-; dark blue (0x4)
-; magenta (0xD)
-COLORS:		.fcb	0x98,0x98
-		.fcb	0xB9,0xB9
-		.fcb	0x3B,0x3B
-		.fcb	0x73,0x73
-		.fcb	0x57,0x57
-		.fcb	0x45,0x45
-		.fcb	0xD4,0xD4
-		.fcb	0x8D,0x8D
+; light green  (0x3)
+; cyan         (0x7)
+; light blue   (0x5)
+; dark blue    (0x4)
+; magenta      (0xD)
+COLORS          FCB     0x98,0x98
+                FCB     0xB9,0xB9
+                FCB     0x3B,0x3B
+                FCB     0x73,0x73
+                FCB     0x57,0x57
+                FCB     0x45,0x45
+                FCB     0xD4,0xD4
+                FCB     0x8D,0x8D
 
 ;------------------------------------------------------------------------------
 ; data structures
 ;------------------------------------------------------------------------------
 
 ; two grid buffers
-GRID1:		.rmb	GRID_SIZE
-GRID2:		.rmb	GRID_SIZE
+GRID1           RMB     GRID_SIZE
+GRID2           RMB     GRID_SIZE
 
