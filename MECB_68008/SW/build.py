59662fb881436a26cb4044dbd7b01f178f40e9f7
len067 <Emil.Lenc@csiro.au>
2025-11-04 07:55:35 +1100

    Updated build

--- MECB_68008/SW/build.py
+++ MECB_68008/SW/build.py
@@ -3,8 +3,19 @@ import numpy as np
 import sys
 import os
 
-for source in ["test_erom", "test_flash", "test_vdp_gfx"]:#, "test_flash", "test_vdp_text", "test_timer", "test_timer1", , "test_vdp_ram", "test_trap", "read_rom"]:
+build_list = ["test_eflash",    # Test FLASH functionality in ROM expansion card
+#    "test_vdp_gfx",             # Test VDP graphics functionality
+#    "test_erom",                # Test reading of ROM expansion card
+#    "test_flash",               # Test FLASH functionality in onboard ROM
+#    "test_vdp_text",            # Test VDP text functionality
+#    "test_timer",               # Test timer and interrupts
+#    "test_timer1",              # Test timer and interrupts (reports vector used)
+#    "test_vdp_ram",             # Test VDP video memory
+#    "test_trap",                # Test trap functionality in Tutor
+#    "read_rom"                  # Test reading onboard ROM
+            ]
+    
+for source in build_list:
     os.system(f"rm {source}.lst {source}.s19")
     os.system(f"vasmm68k_mot -Fsrec -s19 -L {source}.lst src/{source}.asm")
     os.system(f"mv a.out {source}.s19")
-#    os.system(f"cat a.out s19term.txt >{source}.s19")
